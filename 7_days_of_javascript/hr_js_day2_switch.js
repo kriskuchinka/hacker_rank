7d9745620bf1500c87282812b7adda77e47397bd
Kris Kuchinka <kriskuchinka@gmail.com>
2016-11-23 10:51:45 -0800

    Moved folder to more appropriate location.

--- 7_days_of_javascript/hr_js_day2_switch.js
+++ 7_days_of_javascript/hr_js_day2_switch.js
@@ -1,1 +1,39 @@
 console.log("JavaScript is connected.");
+
+// Declaring variable for num for testing purposes
+// (Explained in HTML file)
+var num = 7;
+
+// Begin switch statement
+switch(num) {
+	case 1:
+		console.log("ONE");
+		break;
+	case 2:
+		console.log("TWO");
+		break;
+	case 3:
+		console.log("THREE");
+		break;
+	case 4:
+		console.log("FOUR");
+		break;
+	case 5:
+		console.log("FIVE");
+		break;
+	case 6:
+		console.log("SIX");
+		break;
+	case 7:
+		console.log("SEVEN");
+		break;
+	case 8:
+		console.log("EIGHT");
+		break;
+	case 9:
+		console.log("NINE");
+		break;
+	default:
+		console.log("PLEASE TRY AGAIN");
+		break;
+} // End of switch
