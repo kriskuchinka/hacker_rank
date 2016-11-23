83c967a97baf0da65939efc9ee8fe5f04e15223f
Kris Kuchinka <kriskuchinka@gmail.com>
2016-11-23 11:43:12 -0800

    Solved arithmetic HR challenge.

---
+++ 7_days_of_javascript/hr_js_day2_arithmetic_operators.js
@@ -0,0 +1,18 @@
+console.log("JavaScript is connected.");
+
+var a = 1;
+var b = 2;
+
+var add = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+var inc = ++a;
+var dec = --b;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(inc);
+console.log(dec);
